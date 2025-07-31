aceb82f07dd215d247b01ec788b084c88425b8e5
Ciaran <55831661+Eldred777@users.noreply.github.com>
2025-07-31 16:27:02 +1000

    added dropna parameter to lqn_io
    Formatted and typed

--- src/bulum/io/lqn_io.py
+++ src/bulum/io/lqn_io.py
@@ -1,5 +1,8 @@
 """
-IO functions for IQQM listquan. 
+IO functions for IQQM listquan output files.
+
+This module provides functionality to read IQQM listquan output files,
+which are space-separated text files containing time series data.
 """
 import os
 
@@ -9,28 +12,34 @@ import pandas as pd
 from bulum import utils
 
 
-def read_iqqm_lqn_output(filename, col_name=None, df=None, 
-                         *, data_start_row=7) -> utils.TimeseriesDataframe:
+def read_iqqm_lqn_output(filename: str | os.PathLike, col_name: str | None = None,
+                         df: pd.DataFrame | None = None, *, data_start_row: int = 7,
+                         dropna: bool = False) -> utils.TimeseriesDataframe:
     """
-    Reads the output of IQQM listquan. This is a space-separated is format with
+    Reads the output of IQQM listquan. This is a space-separated format with
     two columns (date, value) and data starting on line 7.
 
     Parameters
     ----------
-    filename 
+    filename : str or :class:`os.PathLike`
         Path to the file to be read.
-    col_name : optional
+    col_name : str, optional
         If supplied, sets the name for the resulting output column, otherwise
-        uses the filename.
-    df : DataFrame, optional
-        If supplied, joins the output to `df`.
+        uses the filename. Default is None.
+    df : :class:`pandas.DataFrame`, optional
+        If supplied, joins the output to `df`. (Does not modify `df`.)
+        Default is None.
     data_start_row : int, optional
         Optionally specify the start row; may be useful for reading other IQQM
-        TEXT outputs.
+        TEXT outputs. Default is 7.
+    dropna : bool, optional
+        Remove NA values from dataframe before reading. Default is False.
 
     Returns
     -------
-    utils.TimeseriesDataframe
+    :class:`~bulum.utils.TimeseriesDataframe`
+        A time series dataframe containing the parsed data with standardized
+        date index and the specified column name.
     """
     # If no df was supplied, instantiate a new one
     if df is None:
@@ -42,11 +51,14 @@ def read_iqqm_lqn_output(filename, col_name=None, df=None,
     temp = pd.read_csv(filename, skiprows=(data_start_row-2),
                        sep=r'\s+', names=["Date", col_name], header=None)
     # temp = utils.set_index_dt(temp, format='%d/%m/%Y')
+    if dropna:
+        temp = temp.dropna()
     temp.set_index(temp.columns[0], inplace=True)
-    temp.index = utils.standardize_datestring_format(temp.index)
-    temp.index.name = "Date"
+    temp.index = utils.standardize_datestring_format(temp.index)  # type: ignore
+    temp.index.name = "Date"  # type: ignore
     temp = temp.replace(r'^\s*$', np.nan, regex=True)
     df = df.join(temp, how="outer").sort_index()
+
     # TODO: THERE IS NO GUARANTEE THAT THE DATES OVERLAP, THEREFORE WE MAY END UP WITH A DATAFRAME WITH INCOMPLETE DATES
     # TODO: I SHOULD MAKE DEFAULT BEHAVIOUR AUTO-DETECT FORMAT DEPENDING ON *TYPE* AND *LOCATION* OF DELIMIT CHARS
     # TODO: In the meantime we use the below to assert that the format of the resulting df meets our minimum standards.
